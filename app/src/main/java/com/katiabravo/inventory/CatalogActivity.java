ce36469b577197225fbc93c2b238517fe456f70f
KatiaBravo <katia@alexbravo.com>
2017-12-28 14:58:06 -0800

    Implemented more parts in all clases and layouts, getting the basic stuff down

--- app/src/main/java/com/katiabravo/inventory/CatalogActivity.java
+++ app/src/main/java/com/katiabravo/inventory/CatalogActivity.java
@@ -1,36 +1,104 @@
 package com.katiabravo.inventory;
 
 import android.app.LoaderManager;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.CursorLoader;
+import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
-public class CatalogActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+import com.katiabravo.inventory.data.ProductContract.ProductEntry;
+
+public class CatalogActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>{
+
+    private static final int PRODUCT_LOADER = 0;
+    ProductCursorAdapter mCursorAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalog);
 
-        FloatingActionButton floatingActionButton = (FloatingActionButton) findViewById(R.id.fab);
-        floatingActionButton.setOnClickListener()
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CatalogActivity.this, EditorActivity.class);
+                startActivity(intent);
+            }
+        });
+        ListView ProductListView = (ListView) findViewById(R.id.list);
+
+        View emptyView = findViewById(R.id.empty_view);
+        ProductListView.setEmptyView(emptyView);
+
+        mCursorAdapter = new ProductCursorAdapter(this, null);
+        ProductListView.setAdapter(mCursorAdapter);
+
+        ProductListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                Intent editIntent = new Intent(CatalogActivity.this, EditorActivity.class);
+
+                Uri currentProductUri = ContentUris.withAppendedId(ProductEntry.CONTENT_URI, id);
+                editIntent.setData(currentProductUri);
+                startActivity(editIntent);
+            }
+        });
+        getLoaderManager().initLoader(PRODUCT_LOADER, null, this );
     }
 
     @Override
-    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
-        return null;
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_catalog, menu);
+        return true;
     }
 
     @Override
-    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
-
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_delete_all_entries) {
+                deleteAllProducts();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
+
+    private void deleteAllProducts(){
+        int rowsDeleted = getContentResolver().delete(ProductEntry.CONTENT_URI, null, null);
+        Log.v("CatalogActivity", rowsDeleted + " rows deleted from inventory database");
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        String[] project = {
+                ProductEntry._ID,
+                ProductEntry.COLUMN_PRODUCT_NAME,
+                ProductEntry.COLUMN_QUANTITY};
+        return new CursorLoader(this,
+                ProductEntry.CONTENT_URI,
+                project,
+                null,
+                null,
+                null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) { mCursorAdapter.swapCursor(data); }
 
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
-
+        mCursorAdapter.swapCursor(null);
     }
 }
+
--- app/src/main/java/com/katiabravo/inventory/data/ProductDbHelper.java
+++ app/src/main/java/com/katiabravo/inventory/data/ProductDbHelper.java
@@ -1,4 +1,31 @@
 package com.katiabravo.inventory.data;
 
-public class ProductDbHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class ProductDbHelper extends SQLiteOpenHelper {
+
+    public static final String LOG_TAG = ProductDbHelper.class.getSimpleName();
+
+    private static final String DATABASE_NAME = "shelter.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public ProductDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String SQL_CREATE_ProductS_TABLE =  "CREATE TABLE "
+                + ProductContract.ProductEntry.TABLE_NAME + " ("
+                + ProductContract.ProductEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + ProductContract.ProductEntry.COLUMN_PRODUCT_NAME + " TEXT NOT NULL, "
+                + ProductContract.ProductEntry.COLUMN_QUANTITY + " TEXT, "
+                + ProductContract.ProductEntry.COLUMN_PRICE + " INTEGER NOT NULL, " + ")";
+        db.execSQL(SQL_CREATE_ProductS_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {}
 }
